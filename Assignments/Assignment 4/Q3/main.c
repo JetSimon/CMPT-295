6e95e4fa2c575edb0e905766afb7ed9c32d9b1e6
Jet Simon <jetjdsimon@gmail.com>
2022-02-14 14:09:44 -0800

    add < sign

--- Assignments/Assignment 4/Q3/main.c
+++ Assignments/Assignment 4/Q3/main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
 		y = atoi(argv[2]);
 
 		result = lessThan(x, y); // Make sure you change the name of this function - see calculator.s
-  		printf("%d ??? %d -> %d\n", x, y, result); // Make sure you change ??? to the appropriate symbol
+  		printf("%d < %d -> %d\n", x, y, result); // Make sure you change ??? to the appropriate symbol
 
 		result = plus(x, y);
   		printf("%d + %d = %d\n", x, y, result);
