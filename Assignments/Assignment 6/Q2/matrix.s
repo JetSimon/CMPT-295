10ba21e482c8532cf1603f6e5c63299b8d418605
Jet Simon <jetjdsimon@gmail.com>
2022-03-10 09:16:37 -0800

    Update matrix.s

--- Assignments/Assignment 6/Q2/matrix.s
+++ Assignments/Assignment 6/Q2/matrix.s
@@ -58,7 +58,9 @@ transpose:
 # For each row
 rowLoop2:
 	movl $0, %r8d			# column number j in r8d -> j = 0
+	sub $2, %esi
 	cmpl %esi, %ecx			# loop as long as i - N < 0
+	add $2, %esi
 	jge doneWithRows2
 
 # For each cell of this row
