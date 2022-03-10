d6288d59294c620efad903f97db9029d567a6336
Jet Simon <jetjdsimon@gmail.com>
2022-03-10 09:13:25 -0800

    Update matrix.s

--- Assignments/Assignment 6/Q2/matrix.s
+++ Assignments/Assignment 6/Q2/matrix.s
@@ -75,8 +75,7 @@ colLoop2:
 	addq %rdi, %r10			# r10 = A + L * (j + i*N)
 
 # Copy A[L * (j + i*N)] to C[L * (j + i*N)]
-	movb %r8b, (%r10)
-
+	movb $0, (%r10)
 	incl %r8d				# column number j++ (in r8d)
 	jmp colLoop2				# go to next cell
 
